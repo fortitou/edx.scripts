e755402a598535805e7eb1f5955b04415e828753
jobsatskills-commit <bfe@esi-informatique.com>
2020-03-17 15:18:49 +0000

    enhance tutor scripts

--- tutor.list-courses.sh
+++ tutor.list-courses.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tutor local run lms dump_course_ids
+tutor local run cms ./manage.py lms dump_course_ids
 # delete
 #tutor local run cms ./manage.py cms delete_course <your_course_id>
 
