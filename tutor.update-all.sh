cb412c1d5ca23722d4815737b87fc53e52fb7d6a
bfe <bfe@jobsatedxtest.imust.org>
2021-03-23 23:18:02 +0000

    tutor update all

--- tutor.update-all.sh
+++ tutor.update-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd "$(tutor config printroot)/env/build/openedx/themes/"
 git pull
+cd "$(tutor config printroot)/env/build/openedx/locale"
+git pull
+
 tutor images build openedx
 tutor local stop
 tutor local start -d
